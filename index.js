6cf390b35a9c68b3237e1cbccd71f64eb40b0eb1
Emircan Güner <72139725+drenyjs@users.noreply.github.com>
2021-03-19 12:49:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const discord = require('discord.js');
+
+const client = new discord.Client({ disableMentions: 'everyone' });
+
+const { Player } = require('discord-player');
+
+client.player = new Player(client);
+client.config = require('./config/bot');
+client.emotes = client.config.emojis;
+client.filters = client.config.filters;
+client.commands = new discord.Collection();
+
+fs.readdirSync('./commands').forEach(dirs => {
+    const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+
+    for (const file of commands) {
+        const command = require(`./commands/${dirs}/${file}`);
+        console.log(`Loading command ${file}`);
+        client.commands.set(command.name.toLowerCase(), command);
+    };
+});
+
+const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
+
+for (const file of events) {
+    console.log(`Loading discord.js event ${file}`);
+    const event = require(`./events/${file}`);
+    client.on(file.split(".")[0], event.bind(null, client));
+};
+
+for (const file of player) {
+    console.log(`Loading discord-player event ${file}`);
+    const event = require(`./player/${file}`);
+    client.player.on(file.split(".")[0], event.bind(null, client));
+};
+
+const activities_list = [
+    "made by drenyjs#5424", 
+    "beta v0.1.0",
+    "webpanel yakında!", 
+    "im developing!"
+    ];
+
+client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+        client.user.setActivity(activities_list[index], { type: 'WATCHING' });
+    }, 10000); // 10 saniyede bir çalışır.
+});
+
+client.login(client.config.discord.token);
